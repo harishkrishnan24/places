fb081ba81d1ce4252a05c6a9c03a18c3047912ca
Harish Krishnan <harishkrishnan1993@gmail.com>
2019-11-12 15:07:51 +0530

    Added sqllite

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -23,27 +23,36 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: const Text(
-            'Got no places yet, start adding some!',
-          ),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(
-                      greatPlaces.items[i].image,
-                    ),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: const Text(
+                    'Got no places yet, start adding some!',
                   ),
-                  title: Text(
-                    greatPlaces.items[i].title,
-                  ),
-                  onTap: () {},
                 ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              greatPlaces.items[i].image,
+                            ),
+                          ),
+                          title: Text(
+                            greatPlaces.items[i].title,
+                          ),
+                          onTap: () {},
+                        ),
+                      ),
               ),
       ),
     );
